5348dbd8e0ec80f2e8782188a7603b82d6462672
Maunducyrus <maunducyrus123@gmail.com>
2025-04-04 22:44:28 +0300

    models

--- employees/application/models.py
+++ employees/application/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    contact = models.CharField(max_length=15)
+    employee_name = models.CharField(max_length=100)
+    employee_email = models.EmailField()
+    employee_contact = models.CharField(max_length=15)
