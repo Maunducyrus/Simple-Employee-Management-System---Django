f2a0193164c70720e703340180c0c72c3642b01d
Maunducyrus <maunducyrus123@gmail.com>
2025-04-05 08:07:58 +0300

    create view

--- employees/application/views.py
+++ employees/application/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Create view
+def create_employee(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
 
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    else:
+        form = EmployeeForm()
+        return render(request, 'create.html', {'form': form})
