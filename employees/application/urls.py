22fc20675de5cf7ad4cfd8a85b7a3778b84e8e07
Maunducyrus <maunducyrus123@gmail.com>
2025-04-05 08:27:20 +0300

    urls

--- employees/application/urls.py
+++ employees/application/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import create_employee, update_employee, delete_employee, list_employees
 urlpatterns = [
+    path('', employee_list, name='employee_list'),
     path('create/', create_employee, name='create'),
-    path('list/', list_employees, name='list'),
     path('update/<int:pk>/', update_employee, name='update'),
     path('delete/<int:pk>/', delete_employee, name='delete'),
 ]
