04b00526aa3a99a7a0f9a72740ffa573803b80ed
Maunducyrus <maunducyrus123@gmail.com>
2025-04-08 16:30:50 +0300

    commit

--- employees/application/views.py
+++ employees/application/views.py
@@ -21,8 +21,8 @@ def list_employees(request):
     return render(request, 'list.html', {'employees': employees})
 
 # Update view
-def update_employee(request, emp_id):
-    employee = Employee.objects.get(employee_id=emp_id)
+def update_employee(request, pk):
+    employee = Employee.objects.get(employee_id=pk)
     if request.method == 'POST':
         form = EmployeeForm(request.POST, instance=employee)
 
